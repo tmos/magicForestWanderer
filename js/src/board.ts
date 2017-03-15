9c5a2ac1a44db6d0c2f8d9ec6d99375c669bcf22
Tom Canac <contact@tomcanac.com>
2017-03-15 13:16:44 -0400

    transcript of the classes of mansionCleaner

--- js/src/board.ts
+++ js/src/board.ts
@@ -1,11 +1,14 @@
+import {deep, deepClue, empty, goal, monster, monsterClue, tree} from "./constants";
+
 export class Forest {
     /**
      * Create a new forest
      * @param {*} width The width of the forest
      * @param {*} height The height of the forest
      */
-    private width = 0;
-    private height = 0;
+    private forest: number[] = [];
+    private width: number = 0;
+    private height: number = 0;
 
     constructor(w = 4, h = 4) {
         this.width = w;
@@ -13,6 +16,17 @@ export class Forest {
     }
 
     public populate(maxChances = 50) {
+        for (let line = 0; line < this.width; line++) {
+            for (let square = 0; square < this.height; square++) {
+                let tmpRand = Math.random() * (maxChances - 0) + 0;
 
+                if (tmpRand === 0) {
+
+                } else if (tmpRand === 1) {
+
+                }
+            }
+
+        }
     }
 }
---
+++ js/src/constants.ts
@@ -0,0 +1,7 @@
+export const deep = "d";
+export const deepClue = "dc";
+export const empty = "";
+export const goal = "g";
+export const monster = "m";
+export const monsterClue = "mc";
+export const tree = "t";
