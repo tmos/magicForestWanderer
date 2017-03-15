42424ee5b48713a92e78f8583d70cfda9ba86dbb
plfaure <plouis.faure@gmail.com>
2017-03-15 14:44:18 -0400

    Floor (3)

--- js/src/Forest.ts
+++ js/src/Forest.ts
@@ -1,4 +1,4 @@
-import {trap, trapClue, empty, goal, monster, monsterClue, tree} from "./constants";
+import {trap, empty, goal, monster, tree} from "./constants";
 import Floor from "./Floor";
 
 /**
@@ -19,6 +19,10 @@ export default class Forest {
         this.height = h;
     }
 
+    /**
+     * Populate the forest randomly
+     * @param {*} maxChances The chances
+     */
     public populate(maxChances = 50) {
         for (let line = 0; line < this.width; line++) {
             for (let square = 0; square < this.height; square++) {
--- js/src/constants.ts
+++ js/src/constants.ts
@@ -1,7 +1,5 @@
-export const trap = "d";
-export const trapClue = "dc";
+export const trap = "trap";
 export const empty = "";
-export const goal = "g";
-export const monster = "m";
-export const monsterClue = "mc";
-export const tree = "t";
+export const goal = "goal";
+export const monster = "monster";
+export const tree = "tree";
